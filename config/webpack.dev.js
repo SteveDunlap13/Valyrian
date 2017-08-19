87fd8eb64260d9d1bc0a2b54593881b69c49ffed
Steve Dunlap <SteveDunlap13@gmail.com>
2017-08-19 10:42:51 -0400

    dev configured for ssl

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -4,24 +4,15 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
 
-var path = require('path');
-var fs = require('fs');
-
-//var cert = fs.readFileSync(path.join(__dirname, "../ValyrianCertificate.crt"));
-//var key = fs.readFileSync(path.join(__dirname, "../ValyrianCertificateKey.pem"));
 
 module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
 
   output: {
     path: helpers.root('dist'),
-    publicPath: 'http://localhost:3123/',
+    publicPath: 'https://localhost:3123/',
     filename: '[name].js',
-    chunkFilename: '[id].chunk.js',
-    //https: {
-    //  cert: cert,
-    //  key: key
-    //}
+    chunkFilename: '[id].chunk.js'
   },
 
   plugins: [
